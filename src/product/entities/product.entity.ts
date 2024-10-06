c718807b72c48d658804637f8bc0ee306f8051fc
jangsen <dh7895210@naver.com>
2024-10-06 12:48:29 +0900

    product Entity init

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from './base.entity';
+
+@Entity()
+export class Product extends BaseEntity{
+  @Column({ unique: true })
+  public name: string;
+
+  @Column()
+  public price: number;
+
+  @Column()
+  public description: string;
+
+  @Column()
+  public category: string;
+
+  @Column()
+  public productImg: string;
+}
